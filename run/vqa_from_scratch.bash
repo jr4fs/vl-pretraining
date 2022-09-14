706298f87895a301af78448a39c75b70acd832bf
jr4fs <jr4fs@virginia.edu>
2022-09-13 20:18:03 -0700

    animal subset training datamaps

---
+++ run/vqa_from_scratch.bash
@@ -0,0 +1,19 @@
+# The name of this experiment.
+source /opt/anaconda3/etc/profile.d/conda.sh
+conda activate lxmert
+name=$2
+
+# Save logs and models under snap/vqa; make backup.
+output=snap/vqa/$name
+mkdir -p $output/src
+cp -r src/* $output/src/
+cp $0 $output/run.bash
+
+# See Readme.md for option details.
+CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=$PYTHONPATH:./src \
+    python src/tasks/vqa.py \
+    --train train,nominival --valid minival  \
+    --llayers 9 --xlayers 5 --rlayers 5 \
+    --fromScratch \
+    --batchSize 32 --optim bert --lr 5e-5 --epochs 10 \
+    --tqdm --output $output ${@:3}
