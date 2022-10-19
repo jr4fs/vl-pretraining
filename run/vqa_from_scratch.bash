8ea71b991228e64e8bd7d33919ed5c619c52d15a
jr4fs <jr4fs@virginia.edu>
2022-10-18 20:23:29 -0700

    experiments

--- run/vqa_from_scratch.bash
+++ run/vqa_from_scratch.bash
@@ -8,7 +8,6 @@ output=snap/vqa/$name
 mkdir -p $output/src
 cp -r src/* $output/src/
 cp $0 $output/run.bash
-
 # See Readme.md for option details.
 CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/vqa.py \
