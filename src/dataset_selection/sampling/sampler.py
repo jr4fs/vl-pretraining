33b93634d2363898bec54b64eb7006ff4411d769
jr4fs <jr4fs@virginia.edu>
2023-02-25 13:06:46 -0800

    sampling

--- src/dataset_selection/sampling/sampler.py
+++ src/dataset_selection/sampling/sampler.py
@@ -71,9 +71,9 @@ if __name__ == "__main__":
                     beta_sampling(df, param[0], param[1], args.sampling_model, budget, norm=norm, bandwidth=args.bandwidth, include_all_classes=args.include_all_classes, dataset=args.sampling_dataset)
     elif sampling_method == 'beta_multilabel':
         #params = [(1, 2), (2, 1), (1, 1),(2, 2)]
-        params = [(8,2), (2,8)]
+        params = [(1, 2), (2, 1), (1, 1),(2, 2), (8,2), (2,8)]
         #norms = ['gaussian', 'tophat', 'epanechnikov', 'exponential', 'linear', 'cosine']
-        norms = ['tophat']
+        norms = ['tophat', 'pvals']
         # norms = ['pvals', 'var_counts', 'gaussian', 'tophat', 'epanechnikov', 'exponential', 'linear', 'cosine']
         for norm in norms:
             print("Norm: ", norm)
